d353066b3c972d7ee55179e2a7063e9ed66eb460
ChrisT33850 <christophe.trevise@acteongroup.com>
2026-09-10 13:44:16 +0200

    Add HelpdeskKanban component with user data loading

--- force-app/main/default/lwc/helpdeskKanban/helpdeskKanban.js
+++ force-app/main/default/lwc/helpdeskKanban/helpdeskKanban.js
@@ -1,1 +1,41 @@
 
+import { LightningElement, wire, track } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+import userId from '@salesforce/user/Id';
+import getUserQueues from '@salesforce/apex/HelpdeskKanbanController.getUserQueues';
+import getQueuesWithCaseCount from '@salesforce/apex/HelpdeskKanbanController.getQueuesWithCaseCount';
+import getCasesForKanban from '@salesforce/apex/HelpdeskKanbanController.getCasesForKanban';
+import getKanbanStatuses from '@salesforce/apex/HelpdeskKanbanController.getKanbanStatuses';
+import updateCaseKanbanStatus from '@salesforce/apex/HelpdeskKanbanController.updateCaseKanbanStatus';
+
+export default class HelpdeskKanban extends LightningElement {
+    userId = userId;
+    
+    // Queues
+    @track queuesWithCount = [];
+    selectedQueueId = null;
+    
+    // Kanban
+    @track kanbanStatuses = [];
+    @track allCases = [];
+    @track casesByStatus = {};
+    
+    // État
+    @track isLoading = true;
+    @track hasError = false;
+    @track errorMessage = '';
+    
+    // Drag & Drop
+    @track draggedCase = null;
+    @track draggedFromStatus = null;
+
+    @wire(getRecord, { recordId: userId, fields: ['User.Id', 'User.Name'] })
+    wiredUser({ error, data }) {
+        if (data) {
+            this.loadInitialData();
+        } else if (error) {
+            this.handleError('Erreur au chargement de l\'utilisateur');
+        }
+    }
+
+    async loadInitialData() {
